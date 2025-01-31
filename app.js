b30520d4057de8d01a8a361bef2f997dbadfdcd7
pmuena <pmuena@fen.uchile.cl>
2025-01-31 23:02:26 +0100

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+//Variables
+//Array de amigosSorteados infinita
 let amigos = [];
 
+function agregarAmigos() {
+    let amigoSorteado = document.getElementById('amigo').value;
+    console.log(amigoSorteado);
+    if (amigoSorteado = '') {
+        alert('Por favor, inserte un nombre');
+    }
+    else {
+    amigos.push('' + amigoSorteado + '');
+    return;
+}
+}
+}
+
+let botonAgregar = document.getElementById('agregar'); 
